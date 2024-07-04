377759cc3720b6117b0eb2e6beb1d9f814330b60
marcusjunior <marcus.junioor@gmail.com>
2024-07-04 18:22:24 -0300

    API checkout added

---
+++ src/main/java/com/br/checkout/application/service/CheckoutService.java
@@ -0,0 +1,33 @@
+package com.br.checkout.application.service;
+
+import com.br.checkout.adapter.messaging.MessagePublisher;
+import com.br.checkout.domain.model.Order;
+import com.br.checkout.domain.usecase.CreateOrderUseCase;
+import com.br.checkout.domain.usecase.GetOrdersUseCase;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CheckoutService {
+
+    private final CreateOrderUseCase createOrderUseCase;
+    private final GetOrdersUseCase getOrdersUseCase;
+    private final MessagePublisher messagePublisher;
+
+    public CheckoutService(CreateOrderUseCase createOrderUseCase, GetOrdersUseCase getOrdersUseCase, MessagePublisher messagePublisher) {
+        this.createOrderUseCase = createOrderUseCase;
+        this.getOrdersUseCase = getOrdersUseCase;
+        this.messagePublisher = messagePublisher;
+    }
+
+    public Order createOrder(Order order) {
+        Order createdOrder = createOrderUseCase.createOrder(order);
+        messagePublisher.publishOrder(createdOrder);
+        return createdOrder;
+    }
+
+    public List<Order> getAllOrders() {
+        return getOrdersUseCase.getAllOrders();
+    }
+}
